062646f5c2bdb5e784457c31f2526f6eb1ac4f2c
Sarah Clifton <90790477+sarahclifton@users.noreply.github.com>
2021-10-15 17:54:40 -0500

    Add files via upload

---
+++ LA.py
@@ -0,0 +1,196 @@
+"""
+This homework is due on 10/15/2021 by 11:59pm. 
+
+
+For this assignment you will be writing a python script to be named LA.py. In
+this script you will need to write 6 functions. Every function must 
+
+1) Have a doc string.
+
+2) Have type annotations
+
+3) Be tested using unit testing. 
+
+Once you have finished writing LA.py you will upload it to the same github repo
+you used for HW02. The functions you need to write are 
+"""
+
+#0
+"""
+A function which takes as it's arguments two vectors stored as
+lists and returns their sum, also stored as a list.
+"""
+
+def add_vectors(vector_a: list[float], vector_b: list[float]) -> list[float]:
+    """Adds the two input vectors.
+
+    Creates a result vector stored as a list of 0's the same length as the input. 
+    Then overwrites each element of the result vector with the corresponding
+    element of the sum of the input vectors. Achieves this using a for loop over
+    the indices of result. 
+
+    Args:
+        vector_a: A vector stored as a list.
+        vector_b: A vector, the same length as vector_a, stored as a list.
+
+    Returns:
+       The sum of the input vectors stored as a list. 
+    """ 
+    result: list[float] = [0 for element in vector_a]
+    for index in range(len(result)):
+        result[index] = vector_a[index] + vector_b[index]
+    return result
+
+# End Example
+# Note that you must add unit tests for problem 0!!!!!
+
+#1
+"""
+A function which takes as it's arguments a vector stored as a list and a
+scalar, and returns the scalar vector multiplication stored as a list.
+"""
+
+def scalar_vec_multi(vector: list[float], scalar: float) -> list[float]:
+    """Multplies the input scalar and the input vector.
+
+    Creates a result vector, called result, as an empty list as a copy of 
+    vector. Then multiplies the scalar by each the elements in the vector 
+    and add it to the result. Return the desire result.
+
+    Args:
+        vector_a: A vector stored as a list.
+        scalar_a: A scalar stored as a number.
+
+    Returns:
+        The product of the input scalar and the input vector stored as a list. 
+    """
+    result: list[float] = []
+    for index in range(len(vector)):
+        result.append(vector[index] * scalar)
+    return result
+
+#2
+"""
+A function which takes as it's arguments a matrix, stored as a list of lists
+where each component list represents a column of the matrix(you cannot represent
+the matrix as a list of rows!) and a scalar and returns their product, also
+stored as a list of lists where each component list represents a column. You
+must use the function from problem #1. Failure to use this function will result
+in an earned grade of 0.
+"""
+
+def scalar_mat_multi(matrix: list[list[float]], scalar: float) -> list[list[float]]:
+    """Multiplies the input scalar and the input matrix.
+
+    Creates a result matrix stored as a list of lists of 0's the same length as the input.
+    Sets each element of the product of the matrix and scalar to the result, using the 
+    scalar_vec_multi function. Replaces the 0's with the product of the inputs. 
+    Repeat this for every corresponding element, and return the desire result.
+
+    Args:
+        matrix_a: A matrix stored as a list of lists. 
+        scalar_a: A scalar stored as a number.
+
+    Returns:
+        The product of the input scalar and the input matrix. 
+    """
+    result: list[list[float]] = [0 for element in matrix]
+    for index in range(len(result)):
+        result[index] = scalar_vec_multi(matrix[index], scalar)
+    return result
+
+#3
+"""
+A function which takes as it's arguments two matrices stored as lists of
+lists where each component list represents a column vector, and returns their
+sum stored in the same manner. You must use the function in problem #0 in your
+method here. Failure to use the function from problem #0 will reuslt in an
+earned grade of 0.
+"""
+
+def add_matrix(matrix_a: list[list[float]], matrix_b: list[list[float]]) -> list[list[float]]:
+    """Adds the two input matrices.
+
+    Creates a result matrix stored as a list of list of 0's the same length as the input.
+    Then overwrites each element of the result vector with the corresponding element of 
+    the sum of the input vectors. Sets each element of the sum of the matrices to the result, 
+    using the add_vectors function. Replaces the 0 with the sum of the inputs, and repeat 
+    this for every corresponding element. Return the desired result
+
+    Args:
+        matrix_a: A matrix stored as a list of lists.
+        matrix_b: A matrix, the same length as matrix_a, stored as a list of lists.
+
+    Returns:
+        The sum of the two input matrices.
+    """
+    result: list[list[float]] = [0 for element in matrix_a]
+    for index in range(len(result)):
+        result[index] = add_vectors(matrix_a[index], matrix_b[index])
+    return result
+
+#4
+"""
+A function which takes as it's argument a matrix (stored as a list of lists,
+each component list representing a column vector), and a vector stored as a
+list, and returns the matrix-vector product. This function must compute the
+matrix-vector product by calculating the neccessary linear combination of the
+input matrices columns. All other methods of matrix-vector multiplication are
+strictly forbidden and their use will result in a grade of 0. For this function
+you must use the functions written for problem #0 and problem #1. Failure to use
+these functions will result in an earned grade of 0.
+"""
+
+def mat_vec_multi(matrix: list[float], vector:list[float]) -> list[list[float]]:
+    """Multiplies the input vector and the input matrix.
+
+    Creates a result matrix stored as a list of list of 0's the same length as the input.
+    Then, overwrites each element with its corresponding  product from the column vector
+    which multiplies the corresponding column of the matrix. For each list in matrix, add
+    the elements of the columns Replaces the 0 with the sum of the inputs, and return the 
+    desired result.
+
+    Args:
+        matrix_a: A matrix stored as a list of lists.
+        vector_c: A vector stored as a list.
+
+    Returns:
+        The product of the input vector and the input matrix. 
+    """
+    mat_result: list[list[float]] = [0 for elements in matrix]
+    for index in range(len(vector)):
+        mat_result[index] = scalar_vec_multi(matrix[index], vector[index])
+    result = add_vectors(mat_result[0], mat_result[1])
+    for index in range(2, len(mat_result)):
+        result = add_vectors(result, mat_result[index])
+    return result
+
+#5
+"""
+A function which takes as it's arguments two matrices, each stored as a list
+of lists where each component list represents a column vector, and returns their
+product stored in the same manner. To earn any credit on this problem you must
+use the function from problem #4 to implement the matrix-vector method of
+matrix-matrix multiplication. Use of any other method will result in an earned
+grade of 0.
+"""
+
+def mat_multi(matrix_a: list[float], matrix_b: list[float]) -> list[list[float]]:
+    """Multiplies the two input matrices.
+
+    Creates a result matrix, called result, as an empty list as a copy of matrix. 
+    Sets each element of the product of the matrices to the result list of lists, 
+    using the mat_vec_multi function. Replaces the 0's with the product of the inputs. 
+    Repeat this for every corresponding element, and return the desire result.
+
+    Args:
+        matrix_a: A matrix stored as a list of lists.
+        matrix_b: A matrix, the same length as matrix_a, stored as a list of lists.
+
+    Returns:
+        The product of the two input matrices. 
+    """
+    result: list[list[float]] = []
+    for index in range(len(matrix_b)):
+         result.append(mat_vec_multi(matrix_a, matrix_b[index]))
+    return result
